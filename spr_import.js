bb2c495859e8fdcc9e1ae75e57f454853a5413c7
75min <minkang1007@gmail.com>
2025-08-11 12:09:50 +0900

    Update spr_import.js

--- spr_import.js
+++ spr_import.js
@@ -6,17 +6,18 @@ const spr_movespine = new Image();
 const spr_enemy = new Image();
 const spr_door = new Image();
 const spr_key = new Image();
-spr_char.src = "spr_char.png"
-spr_wall.src = "spr_wall.png"
-spr_pushwall.src = "spr_pushwall.png"
-spr_spine.src = "spr_spine.png"
-spr_movespine.src = "spr_movespine.png"
-spr_enemy.src = "spr_enemy.png"
-spr_door.src = "spr_door.png"
-spr_key.src = "spr_key.png"
+spr_char.src = "sprites/spr_char.png"
+spr_wall.src = "sprites/spr_wall.png"
+spr_pushwall.src = "sprites/spr_pushwall.png"
+spr_spine.src = "sprites/spr_spine.png"
+spr_movespine.src = "sprites/spr_movespine.png"
+spr_enemy.src = "sprites/spr_enemy.png"
+spr_door.src = "sprites/spr_door.png"
+spr_key.src = "sprites/spr_key.png"
 
 //const spr_anime = new Image();
 
 const bck_tile = new Image();
 
 bck_tile.src = "bck_tile.png"
+
